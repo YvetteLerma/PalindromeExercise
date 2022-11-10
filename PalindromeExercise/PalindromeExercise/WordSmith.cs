e5a9507c1cd023fa0baaaca7e3aebb2826604b76
Yvette Lerma <lerma.yvette@hotmail.com>
2022-11-10 13:26:46 -0700

    study study study

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string values)
+        {
+            //string Reversed = "";
+
+            //for (int i = values.Length - 1; i >= 0; i--) 
+            //{
+            //    Reversed += values[i];
+            //}
+            //if (Reversed == values)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+            return values?.ToLower().SequenceEqual(values?.ToLower().Reverse()) ?? false;
+        }
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,28 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("kayak", true)]
+        [InlineData("KaYaK", true)]
+        [InlineData("madam", true)]
+        [InlineData("potato", false)]
+        [InlineData("hello", false)]
+        [InlineData("RaCeCaR", true)]// ← you will need to put some test data and expected result here.
 
+        public void PalindromeTester(string values, bool expected)
+        {
+            var tester = new WordSmith();
+
+            bool actual = tester.IsAPalindrome(values);
+
+            Assert.Equal(expected, actual);
         }
+        // For example [InlineData(“racecar”, true)]
     }
 }
